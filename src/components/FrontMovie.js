5ae57aa8cd184b3812ef6239665baf1d3f4cc4fe
Francisco <francisco.marquez.solt@gmail.com>
2021-08-11 14:57:47 -0500

    To redux

--- src/components/FrontMovie.js
+++ src/components/FrontMovie.js
@@ -6,27 +6,25 @@ const FrontMovie = ({ title, backdrop_path, overview, homepage }) => {
 
   return (
     <React.Fragment>
-      {
-        <div className="frontMovie--container">
-          <div className="frontMovie__post">
-            <img src={url + backdrop_path} alt="" />
-            <div className="info">
-              <h1>{title}</h1>
-              <p className="description">{overview}</p>
-            </div>
-            <div className="frontMovie__buttons">
-              <button className="btn btn-primary">
-                <a href={homepage}>
-                  <p>+ Info</p>
-                </a>
-              </button>
-              <button className="btn btn-secondary">
-                <p>Watch List</p>
-              </button>
-            </div>
+      <div className="frontMovie--container">
+        <div className="frontMovie__post">
+          <img src={url + backdrop_path} alt="" />
+          <div className="info">
+            <h1>{title}</h1>
+            <p className="description">{overview}</p>
+          </div>
+          <div className="frontMovie__buttons">
+            <button className="btn btn-primary">
+              <a href={homepage}>
+                <p>+ Info</p>
+              </a>
+            </button>
+            <button className="btn btn-secondary">
+              <p>Watch List</p>
+            </button>
           </div>
         </div>
-      }
+      </div>
     </React.Fragment>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { Provider } from "react-redux"; 
+import { createStore } from "redux";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
@@ -5,6 +7,8 @@ import App from './components/App';
 import "./global.css"
 
 const container = document.getElementById('app')
+
+
 
 ReactDOM.render(<App />, container) 
 
