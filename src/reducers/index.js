f54dfd5fbd5494fce9e05e82d6201ff7a7ecd5f7
Francisco <francisco.marquez.solt@gmail.com>
2021-08-19 19:56:39 -0500

    Start with centralize redux state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,14 +12,28 @@ const reducer = (state, action) => {
       const exist = state.myList.find((item) => item.id === action.payload.id);
       if (exist) return { ...state };
       return {
-        myList: [...state.myList, action.payload]
+        myList: [...state.myList, action.payload],
       };
-    
-      case actions.removeFavorite:
+
+    case actions.removeFavorite:
+      return {
+        ...state,
+        myList: state.myList.filter((items) => items.id !== action.payload),
+      };
+
+    case actions.getSearch:
+      if (action.payload === ""){
         return {
           ...state,
-          myList: state.myList.filter((items) => items.id !== action.payload),
-        }
+          searchResult: [],
+        };
+      }
+
+      
+      return {
+        ...state,
+        searchResult: action.payload,
+      };
 
     default:
       return state;
